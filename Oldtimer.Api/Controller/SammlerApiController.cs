234e8f8d87846554d0de63a5486b78999d84a8d3
Orkan <116543534+Orkan-Goekcen@users.noreply.github.com>
2023-08-16 11:34:20 +0200

    Einteilung in 2 Controller

--- Oldtimer.Api/Controller/SammlerApiController.cs
+++ Oldtimer.Api/Controller/SammlerApiController.cs
@@ -4,22 +4,21 @@ using Oldtimer.Api.Commands;
 using Oldtimer.Api.Data;
 using Oldtimer.Api.Queries;
 using Swashbuckle.AspNetCore.Annotations;
-using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
 
 namespace Oldtimer.Api.Controller
 {
     [ApiController]
-    [Route("api")]
-    public class ApiController : ControllerBase
+    [Route("api/sammler")]
+    public class SammlerApiController : ControllerBase
     {
         private readonly IMediator mediator;
 
-        public ApiController(IMediator mediator)
+        public SammlerApiController(IMediator mediator)
         {
             this.mediator = mediator;
         }
 
-        [HttpGet("Sammler")]
+        [HttpGet]
         [SwaggerOperation("Get all Sammlers")]
         public async Task<IActionResult> GetSammlers()
         {
@@ -29,7 +28,7 @@ namespace Oldtimer.Api.Controller
             return Ok(sammler);
         }
 
-        [HttpGet("Sammler/{id}")]
+        [HttpGet("{id}")]
         [SwaggerOperation("Get Sammler by ID")]
         public async Task<IActionResult> GetSammlerById(long id)
         {
@@ -145,91 +144,6 @@ namespace Oldtimer.Api.Controller
             await mediator.Send(updateSammlerCommand);
 
             return Ok(sammler);
-        }
-
-
-        [HttpGet("Oldtimer")]
-        [SwaggerOperation("Get all Oldtimer")]
-        public async Task<IActionResult> GetAllOldtimer()
-        {
-            var query = new GetAllOldtimerQuery { };
-            var oldtimer = await mediator.Send(query);
-
-            return Ok(oldtimer);
-        }
-
-        [HttpGet("Oldtimer/Sammler/{id}")]
-        [SwaggerOperation("Get Oldtimer by Sammler ID")]
-        public async Task<IActionResult> GetOldtimerBySammlerId(long id)
-        {
-            var query = new GetOldtimerPlusSammlerBySammlerIdQuery { SammlerId = id};
-            var oldtimer = await mediator.Send(query);
-
-            if (oldtimer.Count == 0) 
-            {
-                return NotFound($"Id:{id} does not exist!");
-            }
-            return Ok(oldtimer);
-        }
-
-        [HttpGet("Sammler/Oldtimer")]
-        [SwaggerOperation("Get Sammler by Oldtimer Brand and Model")]
-        public async Task<IActionResult> GetSammlerByOldtimerBrandAndModel(string brand, string model)
-        {
-            if (string.IsNullOrWhiteSpace(brand) && string.IsNullOrWhiteSpace(model))
-            {
-                return BadRequest("Both 'brand' and 'model' cannot be empty or null.");
-            }
-
-            if (!string.IsNullOrWhiteSpace(brand))
-            {
-                brand = char.ToUpper(brand[0]) + brand.Substring(1);
-            }
-
-            if (!string.IsNullOrWhiteSpace(model))
-            {
-                model = char.ToUpper(model[0]) + model.Substring(1);
-            }
-
-            var query = new GetSammlerByOldtimerBrandAndModelQuery { Brand = brand, Model = model};
-            var sammler = await mediator.Send(query);
-
-            if (sammler == null || sammler.Count == 0)
-            {
-                return NotFound();
-            }
-
-            return Ok(sammler);
-        }
-
-
-        [HttpPost("Sammler/{id}/Oldtimer")]
-        [SwaggerOperation("Add Oldtimer to Sammler")]
-        public async Task<IActionResult> AddOldtimerToSammler(long id, [FromBody] CarDto carDto)
-        {
-            var query = new GetSammlerByIdQuery { SammlerId = id };
-            var sammler = await mediator.Send(query);
-
-            if (sammler == null)
-            {
-                return NotFound();
-            }
-
-            var addOldtimerToSammlerCommand = new AddOldtimerToSammlerCommand { SammlerId = id, CarDto = carDto};
-            var addedOldtimer = await mediator.Send(addOldtimerToSammlerCommand);
-
-            return Ok(addedOldtimer);
-        }
-
-        [HttpDelete("Oldtimer/{id}")]
-        [SwaggerOperation("Remove Oldtimer by ID")]
-        public async Task<IActionResult> RemoveOldtimer(long id)
-        {
-            var command = new RemoveOldtimerCommand { OldtimerId = id };
-
-            await mediator.Send(command);
-
-            return Ok();
         }
     }
 }
