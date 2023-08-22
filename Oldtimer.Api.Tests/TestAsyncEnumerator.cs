d3f26b9d1f6e8194c5ae5a9ad0e85a1c087a111c
Sarican31 <sarican@geomapping.de>
2023-08-22 08:33:50 +0200

    Seperation der TestDaten und Mock mit den Tests

---
+++ Oldtimer.Api.Tests/TestAsyncEnumerator.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Oldtimer.Api.Tests
+{
+    // Implement the IAsyncEnumerator<T> interface
+    public class TestAsyncEnumerator<T> : IAsyncEnumerator<T>
+    {
+        private readonly IEnumerator<T> _innerEnumerator;
+
+        public TestAsyncEnumerator(IEnumerator<T> innerEnumerator)
+        {
+            _innerEnumerator = innerEnumerator;
+        }
+
+        public ValueTask DisposeAsync()
+        {
+            _innerEnumerator.Dispose();
+            return new ValueTask(Task.CompletedTask);
+        }
+
+        public ValueTask<bool> MoveNextAsync()
+        {
+            return new ValueTask<bool>(Task.FromResult(_innerEnumerator.MoveNext()));
+        }
+
+        public T Current => _innerEnumerator.Current;
+    }
+}
---
+++ Oldtimer.Api.Tests/TestData.cs
@@ -0,0 +1,78 @@
+﻿using Dasync.Collections;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+using Oldtimer.Api.Data;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Oldtimer.Api.Tests
+{
+    public static class TestData
+    {
+        public static List<Car> GetCarsTestData()
+        {
+            var cars = new List<Car>
+            {
+                new Car
+                {
+                    Id = 1,
+                    Sammler = new Sammler
+                    {
+                        Id = 1,
+                        Surname = "Frankenstein",
+                        Firstname = "Frank",
+                        Nickname = "Franko",
+                        Birthdate = new DateTime(1997, 8, 15),
+                        Email = "Frank.Franko@gmail.com",
+                        Telephone = "12424242432314"
+                    },
+                    Brand = "Audi",
+                    Model = "A6",
+                    LicensePlate ="HI AB5255",
+                    YearOfConstruction = "2020",
+                    Colors = Car.Color.Red
+                },
+
+                new Car
+                {
+                    Id = 2,
+                    Sammler = new Sammler
+                    {
+                        Id = 2,
+                        Surname = "Hansemann",
+                        Firstname = "Hans",
+                        Nickname = "Hanso",
+                        Birthdate = new DateTime(1966, 4, 16),
+                        Email = "Hans.Hanso@gmail.com",
+                        Telephone = "4446366663"
+                    },
+                    Brand = "Mercedes",
+                    Model = "CL500",
+                    LicensePlate ="H KJ3033",
+                    YearOfConstruction = "2018",
+                    Colors = Car.Color.Blue
+                },
+            };
+            return cars;
+        }
+
+        public static Mock<IApiContext> GetApiContextMock(List<Car> cars)
+        {
+            var queryableCars = cars.AsQueryable();
+            var dbSetMock = new Mock<DbSet<Car>>();
+
+            dbSetMock.As<IQueryable<Car>>().Setup(m => m.Provider).Returns(queryableCars.Provider);
+            dbSetMock.As<IQueryable<Car>>().Setup(m => m.Expression).Returns(queryableCars.Expression);
+            dbSetMock.As<IQueryable<Car>>().Setup(m => m.ElementType).Returns(queryableCars.ElementType);
+            dbSetMock.As<IQueryable<Car>>().Setup(m => m.GetEnumerator()).Returns(queryableCars.GetEnumerator());
+
+            dbSetMock.As<IAsyncEnumerable<Car>>()
+                .Setup(m => m.GetAsyncEnumerator(It.IsAny<CancellationToken>()))
+                .Returns(new TestAsyncEnumerator<Car>(queryableCars.GetEnumerator()));
+
+            var apiContextMock = new Mock<IApiContext>();
+            apiContextMock.Setup(c => c.Cars).Returns(dbSetMock.Object);
+            return apiContextMock;
+        }
+    }
+}
